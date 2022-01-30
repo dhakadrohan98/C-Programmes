e1612257a8526f0f0800661fd0b94b1d5d1f8664
dhakadrohan98 <48330715+dhakadrohan98@users.noreply.github.com>
2022-01-30 10:24:35 +0530

    Add files via upload

---
+++ Nptel Assignment/Week3.c
@@ -0,0 +1,8 @@
+#define fun(x) (x*x)
+int main()
+{
+  float i;
+  i=64.0/fun(2);
+  printf("%.2f",i);
+  return 0;
+}
---
+++ Nptel Assignment/week1.c
@@ -0,0 +1,6 @@
+main()
+{
+    int x=10;
+    y=x;
+    printf("%d %u %u",y,&x,&y);
+}
